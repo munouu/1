f17bd1e4e860c6430c6f939d9bc361ce5fc3d4cc
munouu <gosha.e.r@mail.ru>
2023-11-15 14:46:05 +0300

    sdfn

--- лабы/лаба 6/2.py
+++ лабы/лаба 6/2.py
@@ -1,7 +1,24 @@
 string = input("Введите свое сочинение: ")
+
+print(f'Ваша строка: {string}')
+
+string+=' '
+ind = 0
+string_1 = []
+
+for el in range(0,len(string)): 
+    if string[el] in ';,. ':
+        string_1+=[string[ind:el]]
+        ind=el
+
+ind = 0
 counter = 0
 
-for el in string:
-    if el in "  ,   ;. ":
+for el in string_1:
+    if el != ' ' and el != '.' and el != ';' and el != ',':
         counter+=1
+
+
 print(counter)
+
+
--- лабы/лаба 6/лаба 6 но не моя.py
+++ лабы/лаба 6/лаба 6 но не моя.py
@@ -9,5 +9,7 @@ hi = 20
 for _ in range(1,count+1):
     sp+=[random.randint(lo,hi)]
 print(sp)
-
+#
+sp_ = [random.randint(lo,hi) for i in range(1,count)]
+print(sp)
 
